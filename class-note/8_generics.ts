df412b5bc773d9565e10fd323478400fc10d4c76
enitechajin <purenarim@hanmail.net>
2022-12-06 22:08:28 +0900

    a

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -23,8 +23,63 @@ logText("메롱"); //메롱
 logText("ㅎㄴㅁㄹㅇ"); //문자열반환
 logText('fadsfa');//
 
+//인터페이스에 제네릭을 선언하는 방법
+interface Dropdown {
+    value:string;
+    selected:boolean;
+}
+
+//인터페이스에 제네릭선언
+interface Dropdown2<T> {
+value:T;
+selected:boolean;
+}
 
 
+const obj: Dropdown = {value: '10'd, selected: false};
+
+const obj2:Dropdown2<string> = {value:'abc',selected:false};
+
+//const obj3:Dropdown2<number> = {value:'abc' ,selected:false};
+
+// 제네릭의 타입 제한
+function logTextLength<T>(text:T[]):T[] {
+    console.log(text.length);//배열의 특정 속성에 접근할 수 있다
+    text.forEach(function(text){
+        console.log(text);
+    })
+    return text;
+}
+logTextLength<string>(['hi','abc']);
+
+//제네릭 타입제한 2- 정의된 타입 이용하기
+interface LengthType{
+    length: number;
+}
+
+function logTextLength2<T extends LengthType>(text:T):T {
+    text.length;
+    return text;
+}
+
+logTextLength2('a');
+logTextLength2(10);//generic type 제한
+logTextLength2({length:10});//generic type 제한
+logTextLength2({leng:10});//generic type 제한
 
 
+//generic type 제한 3- keyof
+interface ShoppingItem {
+name:string;
+price:number;
+stock:number;
+}
 
+function getShoppingItemOption<T extends keyof ShoppingItem>(items:T):T{
+    return items;
+}
+
+getShoppingItemOption(10);
+getShoppingItemOption<string>('10');
+
+getShoppingItemOption('name');
--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,16 +1,32 @@
-const emails = [
+// interface Email<T> {
+//   value:T;
+//   selected:boolean;
+// }
+
+interface DropdownItem<T> {
+  value:T;
+  selected:boolean;
+}
+
+const emails: DropdownItem<string>[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-const numberOfProducts = [
+// interface ProductNumber {
+//   value:number;
+//   selected:boolean;
+// }
+
+
+const numberOfProducts: DropdownItem<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item) {
+function createDropdownItem(item: DropdownItem<string>| DropdownItem<number>) {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -19,8 +35,14 @@ function createDropdownItem(item) {
 }
 
 // NOTE: 이메일 드롭 다운 아이템 추가
+
 emails.forEach(function (email) {
   const item = createDropdownItem(email);
-  const selectTag = document.querySelector('#email-dropdown');
+  const selectTag = document.getElementById('email-dropdown');
   selectTag.appendChild(item);
+});
+
+
+numberOfProducts.forEach(function (product) {
+  const item = createDropdownItem(product);
 });
