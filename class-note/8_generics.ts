b32b17f356818ae157c7e99bae515627659e6135
enitechajin <purenarim@hanmail.net>
2022-12-06 22:08:21 +0900

    배우고있다

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -1,7 +1,23 @@
-function logText(text:string){
+
+
+
+
+let arr = [1,2,3,4,5];
+
+
+// function logText2<T>(text:T):T{
+//     console.log(text);
+//     return text;
+// }
+
+// logText2<string>('하이');
+// logText2(arr);
+
+function logText(text){
     console.log(text);
     return text;
 }
+
 
 logText("메롱"); //메롱
 logText("ㅎㄴㅁㄹㅇ"); //문자열반환
@@ -9,13 +25,6 @@ logText('fadsfa');//
 
 
 
-let arr = [1,2,3,4,5];
 
 
-function logText2<T>(text:T):T{
-    console.log(text);
-    return text;
-}
 
-logText2<string>('하이');
-logText2(arr);
